630b1f805a98e98559e698055383ed5e91a2be8e
Davy Jones <andywilkinson1993@gmail.com>
2023-05-26 09:50:43 +0100

    init

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Andrew Wilkinson | Web3/Crypto & AI</title>
+        <title>ADW | Web3/Crypto & AI</title>
         <meta name="title" content="Andrew Wilkinson: Web3/Crypto & AI" />
         <meta
           name="description"
