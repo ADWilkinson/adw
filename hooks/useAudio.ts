3e1fcf65bdaa8e1a104fe80c0fc7e8b350cce28d
Davy jones <hello@galleon.community>
2023-11-19 11:13:00 +0000

    update hook

--- hooks/useAudio.ts
+++ hooks/useAudio.ts
@@ -1,38 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-type UseAudioReturnType = [boolean, () => void];
+type UseAudioReturnType = [boolean, () => void]
 
 const useAudio = (url: string): UseAudioReturnType => {
-  const [audio, setAudio] = useState<HTMLAudioElement | null>(new Audio(url));
-  const [playing, setPlaying] = useState<boolean>(false);
+  const [audio, setAudio] = useState<HTMLAudioElement | null>(
+    typeof Audio !== 'undefined' && new Audio(url)
+  )
+  const [playing, setPlaying] = useState<boolean>(false)
 
-  const toggle = () => setPlaying(!playing);
+  const toggle = () => setPlaying(!playing)
 
   useEffect(() => {
-    const currentAudio = audio;
-    
-    const handleEnded = () => setPlaying(false);
-    currentAudio?.addEventListener('ended', handleEnded);
+    const currentAudio = audio
+
+    const handleEnded = () => setPlaying(false)
+    currentAudio?.addEventListener('ended', handleEnded)
 
     return () => {
-      currentAudio?.removeEventListener('ended', handleEnded);
-    };
-  }, [audio]);
-
-  useEffect(() => {
-    if (audio === null) return;
-    playing ? audio.play().catch(e => console.error('Error playing audio:', e)) : audio.pause();
-  }, [playing, audio]);
-
-  useEffect(() => {
-    const newAudio = new Audio(url);
-    setAudio(newAudio);
-    return () => {
-      newAudio.pause();
+      currentAudio?.removeEventListener('ended', handleEnded)
     }
-  }, [url]);
+  }, [audio])
 
-  return [playing, toggle];
-};
+  useEffect(() => {
+    if (audio === null) return
+    playing
+      ? audio.play().catch((e) => console.error('Error playing audio:', e))
+      : audio.pause()
+  }, [playing])
 
-export default useAudio;
+  useEffect(() => {
+    const newAudio = new Audio(url)
+    setAudio(newAudio)
+    return () => {
+      newAudio.pause()
+    }
+  }, [url])
+
+  return [playing, toggle]
+}
+
+export default useAudio
